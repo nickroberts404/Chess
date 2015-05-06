53e9c15ac380bb72e61c3de999e5d479ee613ce7
nickroberts404 <nickroberts404@gmail.com>
2015-05-06 11:27:32 -0500

    Created project boiler plate and downloaded dependencies.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		concat: {
+			// options: {
+			// 	separator: ';',
+			// },
+			js: {
+				src: ['src/js/*'],
+				dest: 'dist/js/app.js'
+			}
+		},
+	sass: {
+		options: {
+			noCache: true
+		},
+		dist: {
+			files: {
+				'dist/css/main.css': 'src/css/main.scss'
+			}
+		}
+	},
+	watch: {
+		js: {
+			files: ['src/js/*.js'],
+			tasks: ['concat']
+		},
+		css: {
+			files: ['src/css/**/*.scss'],
+			tasks: ['sass']
+		},
+	}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.registerTask('default', ['concat', 'watch', 'sass']);
+
+}
